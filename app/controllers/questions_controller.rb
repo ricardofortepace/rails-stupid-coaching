d1baf211d46bfb35e6fcffc246beb3d24a41a6d6
Ricardo Forte Pace <tatopace@hotmail.com>
2022-02-21 15:27:12 -0300

    Finishing the 02

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = nil
+    @ask = params[:ask]
+
+    if @ask == "I am going to work"
+      @answer =  "Great!"
+    elsif
+      @ask.last == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
